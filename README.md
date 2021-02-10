9289a798d3a89d2f4990d86eb14d87073d40fac1
OvchinnikovMaxim <maxblackwolfovchinnikov@mail.ru>
2021-02-10 12:38:48 +0300

    correcting
    correcting

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # bat-for-deleting-folders-by-mask
+command for search and deleting folders by mask. self remove bat-file
---
+++ delete folders.bat
@@ -0,0 +1,34 @@
+
+cd C:\Users\%username%\AppData\Local\1C\1Cv8
+
+For /F "delims=" %%d in ('dir /b /ad "*-*"') do (
+  takeown /f "%%d" /r /d y
+  echo y|cacls "%%d" /g "%username%":f
+  rd /s /q "%%d"
+)
+
+cd C:\Users\%username%\AppData\Roaming\1C\1Cv8
+
+For /F "delims=" %%d in ('dir /b /ad "*-*"') do (
+  takeown /f "%%d" /r /d y
+  echo y|cacls "%%d" /g "%username%":f
+  rd /s /q "%%d"
+)
+
+cd C:\Users\%username%\AppData\Local\1C\1Cv82
+
+For /F "delims=" %%d in ('dir /b /ad "*-*"') do (
+  takeown /f "%%d" /r /d y
+  echo y|cacls "%%d" /g "%username%":f
+  rd /s /q "%%d"
+)
+
+cd C:\Users\%username%\AppData\Roaming\1C\1Cv82
+
+For /F "delims=" %%d in ('dir /b /ad "*-*"') do (
+  takeown /f "%%d" /r /d y
+  echo y|cacls "%%d" /g "%username%":f
+  rd /s /q "%%d"
+)
+
+del %0
